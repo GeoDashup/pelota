deed1aa5dc06eb0ece051011fb05217882589ffd
GeoDashup <juanismael06@gmail.com>
2015-08-16 19:00:36 +0200

    Repetir pelota y cajas en el escenario
    Repetir pelota y cajas en el escenario

--- main.lua
+++ main.lua
@@ -18,15 +18,24 @@ physics.start( )
 local sky = display.newImage("Clouds.png", centerX, 300 )
 local ground = display.newImage("ground.jpg", centerX, 600 )
 
+--local crate = display.newImage("crate.jpg", centerX, 200)
+
+
 --Activamos suelo para el rebote
 physics.addBody (ground, "static", {friction=0.49 ,bounce=0.56 } )
 
 
 --Creación de la pelota
 
+
+for i=1, 50 do
+
 local pelota = display.newCircle( 130, -70, 20 )
+local crate = display.newImage("crate.jpg", centerX, 200)
+
+physics.addBody( pelota, {density = 0.0, friction=0.2 ,bounce=0.100 } )
+physics.addBody( crate, {density = 0.0, friction=0.2 ,bounce=0.200 } )
 
 
-physics.addBody( pelota, {density = 0.0, friction=0.2 ,bounce=0.6 } )
 
-
+end
